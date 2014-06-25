6987885598e3b3fbc82de9ae8bf0db8a8c7990b0
Wille Marcel <wille@wille.blog.br>
2014-06-25 16:09:42 -0300

    add event_date templatetag

--- happening/templatetags/happening_tags.py
+++ happening/templatetags/happening_tags.py
@@ -6,4 +6,12 @@ register = template.Library()
 
 @register.inclusion_tag('tags/event_cards.html')
 def show_events(event_list):
+    """Render each event on a bootstrap thumbnail"""
     return {'event_list': event_list}
+
+
+@register.inclusion_tag('tags/event_date.html')
+def show_date(start_date, end_date):
+    """Render the date in a especific format depending if the start and end of
+    the event are on the same day, month or year."""
+    return {'start_date': start_date, 'end_date': end_date}
